97ef3c2ef08070c1c5afbe7d4ae771bf948cb085
compsci <compsci@compsci>
2017-10-18 10:21:01 -0600

    Finished nibbles and bytes. Try again loops.

--- src/HexDecBin.java
+++ src/HexDecBin.java
@@ -20,6 +20,7 @@ public class HexDecBin {
 	String answer = "";
 	String msg = "";
 	Boolean playing = true;
+	Boolean typo = true;
 	
 	public HexDecBin() {
 		msg="Welcome to my Hexadecimal, Decimal, or Binary conversion program!";
@@ -32,6 +33,8 @@ public class HexDecBin {
 	
 	public void setNumber() {
 		while (playing == true) {
+		while (typo == true) {
+		typo = false;
 		msg="What is your starting base: hex, dec, or bin?";
 		startbase = JOptionPane.showInputDialog(msg);
 		msg="What base do you want to convert to: hex, dec, or bin?";
@@ -57,8 +60,11 @@ public class HexDecBin {
 			num = Integer.parseInt(bin, 2);
 			msg="You have entered bin "+bin;
 			JOptionPane.showMessageDialog(null, msg);
+		} else {
+			msg = "I don't know what you mean, try again.";
+			JOptionPane.showMessageDialog(null, msg);
+			typo = true;
 		}
-
 		if (endbase.equalsIgnoreCase("dec")) {
 			msg="Decimal value is: "+num+" (remember 0-"+num+" has "+(num+1)+" values)";
 			JOptionPane.showMessageDialog(null, msg);
@@ -68,25 +74,27 @@ public class HexDecBin {
 		JOptionPane.showMessageDialog(null, msg);
 		msg="Number of bits is "+binary.length();
 		JOptionPane.showMessageDialog(null, msg);
-		msg="Number of whole nibbles (4 bits) is x with y bits left over";
+		msg="Number of whole nibbles (4 bits) is "+binary.length()/4+" with "+binary.length()%4+" bits left over";
 		JOptionPane.showMessageDialog(null, msg);
-		msg="Number of whole bytes (8 bits) is x with y bits remaining";
+		msg="Number of whole bytes (8 bits) is "+binary.length()/8+" with "+binary.length()%8+" bits remaining";
 		JOptionPane.showMessageDialog(null, msg);
 		} else if (endbase.equalsIgnoreCase("hex")) {
 		String hex = Integer.toHexString(num);
 		msg="Hexidecimal value is: "+hex;
 		JOptionPane.showMessageDialog(null, msg);
+		} else {
+			msg = "I don't know what you mean, try again.";
+			JOptionPane.showMessageDialog(null, msg);
+			typo = true;
+		}
 		}
 		msg = "Keep playing? y/n";
 		answer = JOptionPane.showInputDialog(msg);
-		if (answer.equalsIgnoreCase("n")) {
+		if (answer.equalsIgnoreCase("n")||answer.equalsIgnoreCase("no")) {
 			playing = false;
-		}else if (answer.equalsIgnoreCase("no")) {
-			playing = false;
-		}else if (answer.equalsIgnoreCase("y")) {
+		}else if (answer.equalsIgnoreCase("y")||answer.equalsIgnoreCase("yes")) {
 			playing = true;
-		}else if (answer.equalsIgnoreCase("yes")) {
-			playing = true;
+			typo = true;
 		}else {
 			msg = "I don't know what you mean? Try again";
 			JOptionPane.showMessageDialog(null, msg);
